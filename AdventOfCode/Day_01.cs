6ab6b805c31a78ca1ebbef4fd2080bb9f41edf26
Anders Moldin <anders.moldin@lakemedelsverket.se>
2021-12-02 16:16:19 +0100

    Solved Part 1

---
+++ AdventOfCode/Day_01.cs
@@ -0,0 +1,35 @@
+﻿namespace AdventOfCode;
+
+public class Day_01 : BaseDay
+{
+    private readonly string _input;
+
+    public Day_01()
+    {
+        _input = File.ReadAllText(InputFilePath);
+    }
+
+    public override async ValueTask<string> Solve_1() => new(await Part1());
+
+    public override async ValueTask<string> Solve_2() => new(await Part2());
+
+    private async Task<string> Part1()
+    {
+        return _input.Split(Environment.NewLine)
+            .Select(int.Parse)
+            .Pairwise((a, b) => a < b ? 1 : 0)
+            .Sum()
+            .ToString();
+    }
+
+    private async Task<string> Part2()
+    {
+        return _input.Split(Environment.NewLine)
+            .Select(int.Parse)
+            .Window(3)
+            .Select(w => w.Sum())
+            .Pairwise((a, b) => a < b ? 1 : 0)
+            .Sum()
+            .ToString();
+    }
+}
