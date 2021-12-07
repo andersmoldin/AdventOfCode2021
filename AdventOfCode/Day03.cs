b5489b39be2b0d7dfbbaf0216607369e8021edbf
Anders Moldin <anders.moldin@lakemedelsverket.se>
2021-12-07 16:26:30 +0100

    Solved Day 3, part 2

--- AdventOfCode/Day03.cs
+++ AdventOfCode/Day03.cs
@@ -1,28 +1,21 @@
-﻿using System.Text;
-
-namespace AdventOfCode;
+﻿namespace AdventOfCode;
 
 public class Day03 : BaseDay
 {
-    private readonly string _input;
+    private readonly string[] _input;
 
     public Day03()
     {
-        _input = File.ReadAllText(InputFilePath);
+        _input = File.ReadAllText(InputFilePath).Split(Environment.NewLine);
     }
 
-    public override async ValueTask<string> Solve_1() => new(await Part1());
-
-    public override async ValueTask<string> Solve_2() => new(await Part2());
-
-    private async Task<string> Part1()
+    public override ValueTask<string> Solve_1()
     {
-        var gammaRateBinary = new string(_input.Split(Environment.NewLine)
+        var gammaRateBinary = new string(_input
             .Transpose()
             .Select(s => s.GroupBy(c => c)
             .OrderByDescending(x => x.Count()).First().Key)
-            .ToArray()
-            );
+            .ToArray());
 
         var gammaRate = Convert.ToInt32(gammaRateBinary, 2);
 
@@ -30,11 +23,43 @@ public class Day03 : BaseDay
 
         var epsilonRate = Convert.ToInt32(epsilonRateBinary, 2);
 
-        return (gammaRate * epsilonRate).ToString();
+        return new((gammaRate * epsilonRate).ToString());
     }
 
-    private async Task<string> Part2()
+    public override ValueTask<string> Solve_2()
     {
-        return "part2";
+        var oxygenGeneratorRating = Convert.ToInt32(GetRating(_input, 0, '1'), 2);
+        var co2ScrubberRating = Convert.ToInt32(GetRating(_input, 0, '0'), 2);
+
+        return new((oxygenGeneratorRating * co2ScrubberRating).ToString());
+
+        string GetRating(IEnumerable<string> input, int position, char masterBit)
+        {
+            if (input.Count() == 1)
+            {
+                return input.Single();
+            }
+
+            var mostCommon = CountOnesAndZeros(input, masterBit);
+
+            return GetRating(input.Where(s => s[position] == mostCommon), position + 1, masterBit);
+
+            char CountOnesAndZeros(IEnumerable<string> input, char masterBit)
+            {
+                var count = input
+                    .Transpose()
+                    .Select(s => new string(s.ToArray())).ToList()
+                    .Select(s => s.GroupBy(c => c).ToDictionary(s => s.Key, s => s.Count()))
+                    .Skip(position)
+                    .First();
+
+                return count['0'] != count['1'] ?
+                    count.Aggregate((x, y) => 
+                        masterBit == '0' ?
+                            x.Value < y.Value ? x : y :
+                            x.Value > y.Value ? x : y).Key :
+                    masterBit;
+            }
+        }
     }
 }
