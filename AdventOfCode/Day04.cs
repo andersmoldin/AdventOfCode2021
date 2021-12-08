ca02486a97f2cc01492a3d1c3fee8eca5c4d98c8
Anders Moldin <anders.moldin@lakemedelsverket.se>
2021-12-08 14:23:30 +0100

    Solved Day 4, Part 2

--- AdventOfCode/Day04.cs
+++ AdventOfCode/Day04.cs
@@ -6,7 +6,6 @@ public class Day04 : BaseDay
 {
     private readonly List<string> _input;
     private readonly List<int> _bingoNumbers;
-    private readonly List<List<List<int>>> _bingoBoards;
 
     public Day04()
     {
@@ -18,8 +17,23 @@ public class Day04 : BaseDay
             .Split(',')
             .Select(int.Parse)
             .ToList();
+    }
 
-        _bingoBoards = _input
+    public override ValueTask<string> Solve_1()
+    {
+        var bingoBoards = GetBingoBoards(_input);
+        return PlayBingo(_bingoNumbers, bingoBoards, Find.First);
+    }
+
+    public override ValueTask<string> Solve_2()
+    {
+        var bingoBoards = GetBingoBoards(_input);
+        return PlayBingo(_bingoNumbers, bingoBoards, Find.Last);
+    }
+
+    List<List<List<int>>> GetBingoBoards(List<string> input)
+    {
+        return input
             .Skip(1)
             .Batch(5)
             .Select(r => r
@@ -30,50 +44,99 @@ public class Day04 : BaseDay
             .ToList();
     }
 
-    public override ValueTask<string> Solve_1()
+    ValueTask<string> PlayBingo(List<int> bingoNumbers, List<List<List<int>>> bingoBoards, Find find)
     {
-        for (int i = 0; i < _bingoNumbers.Count; i++)
+        var register = new Dictionary<int, bool>();
+
+        if (find == Find.Last)
         {
-            for (int j = 0; j < _bingoBoards.Count; j++)
+            for (int i = 0; i < bingoBoards.Count; i++)
             {
-                for (int k = 0; k < _bingoBoards[j].Count; k++)
+                register.Add(i, false);
+            }
+        }
+
+        for (int i = 0; i < bingoNumbers.Count; i++)
+        {
+            for (int j = 0; j < bingoBoards.Count; j++)
+            {
+                for (int k = 0; k < bingoBoards[j].Count; k++)
                 {
-                    for (int l = 0; l < _bingoBoards[j][k].Count; l++)
+                    for (int l = 0; l < bingoBoards[j][k].Count; l++)
                     {
-                        if (_bingoBoards[j][k][l] == _bingoNumbers[i])
+                        if (bingoBoards[j][k][l] == bingoNumbers[i])
                         {
-                            _bingoBoards[j][k][l] = -1;
+                            bingoBoards[j][k][l] = -1;
                         }
                     }
                 }
 
             }
 
-            var bingo = Bingo(_bingoBoards);
+            int bingo = -1;
+            switch (find)
+            {
+                case Find.First:
+                    bingo = Bingo(bingoBoards, find);
+                    break;
+                case Find.Last:
+                    bingo = Bingo(bingoBoards, find, register);
+                    break;
+            }
             if (bingo != -1)
             {
-                return new((bingo * _bingoNumbers[i]).ToString());
+                return new((bingo * bingoNumbers[i]).ToString());
             }
         }
 
         return new("No one won!");
-
-        int Bingo(List<List<List<int>>> boards)
-        {
-            foreach (var board in boards)
-            {
-                if (board.Any(r => r.Sum() == -5) || board.Transpose().Any(r => r.Sum() == -5))
-                {
-                    return board.SelectMany(n => n).Select(n => n).Where(n => n >= 0).Sum();
-                }
-            }
-
-            return -1;
-        }
     }
 
-    public override ValueTask<string> Solve_2()
+    int Bingo(List<List<List<int>>> boards, Find find, Dictionary<int, bool> register = null)
     {
-        return new("Part 2");
+        switch (find)
+        {
+            case Find.First:
+                foreach (var board in boards)
+                {
+                    if (board.Any(r => r.Sum() == -5) || board.Transpose().Any(r => r.Sum() == -5))
+                    {
+                        return board.SelectMany(n => n).Select(n => n).Where(n => n >= 0).Sum();
+                    }
+                }
+                break;
+
+            case Find.Last:
+
+                if (boards.All(b => b.Any(r => r.Sum() == -5) || b.Transpose().Any(r => r.Sum() == -5)))
+                {
+                    return boards[register.Single(b => !b.Value).Key].SelectMany(n => n).Select(n => n).Where(n => n >= 0).Sum();
+                }
+
+                for (int i = 0; i < boards.Count; i++)
+                {
+                    if (boards[i].Any(r => r.Sum() == -5) || boards[i].Transpose().Any(r => r.Sum() == -5))
+                    {
+                        register[i] = true;
+                        //if (register.Values.Count(b => !b) == 1)
+                        //{
+                        //    return boards[register.Single(b => !b.Value).Key].SelectMany(n => n).Select(n => n).Where(n => n >= 0).Sum();
+                        //}
+                        //else
+                        //{
+                        //}
+                    }
+                }
+
+                break;
+        }
+
+        return -1;
+    }
+
+    enum Find
+    {
+        First,
+        Last
     }
 }
